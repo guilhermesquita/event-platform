5859204558a8e8064a305c689121c6caded971e7
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-06-21 17:22:32 -0400

    integration API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY
+
 function App() {
-  
+  useEffect(()=>{
+    client.query()
+  }, [])
+
   return (
     <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
   )
