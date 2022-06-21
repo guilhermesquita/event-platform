4e9ba63203c02d182bee01a7dd14651741b188f2
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-06-21 17:37:38 -0400

    api and apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+import { gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
 import { client } from "./lib/apollo"
 
-const GET_LESSONS_QUERY
+interface Lesson{
+  id: string;
+  title: string;
+}
+
+const GET_LESSONS_QUERY = gql `
+  query{
+    lessons{
+      id
+      title
+    }
+  }
+`
 
 function App() {
-  useEffect(()=>{
-    client.query()
-  }, [])
+
+  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
 
   return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
-  )
-}
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+    )
+  }
 
 export default App
